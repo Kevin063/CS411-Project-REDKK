87847079aaf70b02c1f7991fb0e92037bcf1021e
Eilsiz-Jin <106403253+Eilsiz-Jin@users.noreply.github.com>
2022-12-04 15:01:48 -0500

    new search bar
    and new search bar and set its css

--- relativity-radio/src/app/app.module.ts
+++ relativity-radio/src/app/app.module.ts
@@ -1,15 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule,Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { GoogleMapsModule } from '@angular/google-maps'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GeoRelativityComponent } from './geo-relativity/geo-relativity.component';
+import { PipesPipe } from './pipes.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GeoRelativityComponent,
+    PipesPipe
   ],
   imports: [
     BrowserModule,
+    GoogleMapsModule,
     AppRoutingModule
   ],
   providers: [],
