00e45d7ea7efd176fb5edfdef72ab12163c281b3
Reid Chave <leer_key@protonmail.com>
2022-12-04 23:41:42 -0500

    fiddling around

--- relativity-radio/src/app/app.component.ts
+++ relativity-radio/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'; // OnInit lifecycle hook
+import {HttpClient, HttpHeaders} from '@angular/common/http'; //API
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private http: HttpClient) {} // also for API
+
   title = 'relativity-radio';
+
+  ngOnInit() {
+    // API Call!   TODO: make it actually work!
+
+
+    let headers = new HttpHeaders('', {
+      'x-rapidapi-host': '',
+      'x-rapidapi-key': '',
+    });
+
+    this.http.get<any>({
+
+    }).subscribe(data => {
+      console.log(data);
+    })
+
+
+  }
 }
--- relativity-radio/src/app/app.module.ts
+++ relativity-radio/src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http'; // importing the http module (for API requests)
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule // adding the thing in imports too!
   ],
   providers: [],
   bootstrap: [AppComponent]
