506cfdfd98b0a9a5271670c0a9e1e9bf5024694c
Reid Chave <leer_key@protonmail.com>
2022-12-08 06:33:32 -0500

    more tidying up; compartmentalized components

---
+++ relativity-radio/src/app/title-bar/title-bar.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-title-bar',
+  templateUrl: './title-bar.component.html',
+  styleUrls: ['./title-bar.component.css']
+})
+export class TitleBarComponent implements OnInit { }
