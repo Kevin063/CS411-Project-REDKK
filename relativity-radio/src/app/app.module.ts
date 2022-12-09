90234bd0d707e9c0a5f9b0327a930b7c809dc598
Reid Chave <leer_key@protonmail.com>
2022-12-09 04:24:00 -0500

    organization

--- relativity-radio/src/app/app.module.ts
+++ relativity-radio/src/app/app.module.ts
@@ -17,20 +17,20 @@ import { PipesPipe } from './pipes.pipe';
 
 // sub-components
 import { ControlPanelComponent } from './control-panel/control-panel.component';
-import { GeoRelativityComponent } from './geo-relativity/geo-relativity.component'; // probably gonna make this one a service
 import { SettingsComponent } from './settings/settings.component';
 import { HistoryComponent } from './history/history.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PipesPipe,
-    GeoRelativityComponent,
     ControlPanelComponent,
     SettingsComponent,
     HistoryComponent,
     HomeComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
@@ -40,9 +40,12 @@ import { HomeComponent } from './home/home.component';
     // Loader,
     // LoaderOptions,
     RouterModule.forRoot([
-        { path: '', component: ControlPanelComponent },
+        { path: '', component: HomeComponent },
         { path: 'settings', component: SettingsComponent },
         { path: 'history', component: HistoryComponent },
+        { path: 'panel', component: ControlPanelComponent },
+        { path: 'login', component: LoginComponent },
+      ]),
       ]),
     HttpClientModule,
   ],
